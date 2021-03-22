3126864074ff79880df185ad29a75fca03bdec2b
Alex Lehmann <lehmann.alexander.m@gmail.com>
2021-03-22 09:39:41 -0400

    Fix duplicated code

--- fn/count_decimals.R
+++ fn/count_decimals.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(tidyverse)
 
 count_decimals = function(x) {
@@ -15,21 +14,3 @@ count_decimals = function(x) {
     str_length()
   return(l)
 }
-=======
-library(tidyverse)
-
-count_decimals = function(x) {
-  # Check that input is numeric
-  if (!is.numeric(x) | is.na(x)) {
-    message("Input not numeric; returning NA")
-    return(NA)
-  }
-  
-  # Count nutmber of characters after decimal point
-  l = x %>%
-    as.character() %>%
-    str_extract("(?<=\\.).*") %>%
-    str_length()
-  return(l)
-}
->>>>>>> de130303d9aec92d2e613f5db469811bedd8cca3
--- global.R
+++ global.R
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 # Packages =====================================================================
 library(tidyverse)
 library(tidymodels)
 library(plotly)
 
 # Functions ====================================================================
-=======
-# Packages =====================================================================
-library(tidyverse)
-library(tidymodels)
-library(plotly)
-
-# Functions ====================================================================
->>>>>>> de130303d9aec92d2e613f5db469811bedd8cca3
 source("fn/count_decimals.R")
